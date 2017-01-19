6a199d8c96ed1422b9f21e1bb8e67aa8e485bbc7
Lumuy <dxwang68@126.com>
2017-01-19 16:38:57 +0800

    update appoinment logic and add machine connect validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,20 +1,28 @@
+require 'webmock'
+require 'rest-client'
+require 'nokogiri'
+include WebMock::API
+WebMock.enable!
+WebMock.allow_net_connect!
+
 class Appointment < ApplicationRecord
   validates :id_number, presence: true, format: { with: /\A[1-9][0-9]{16}[0-9X]\Z/ }
 
-  validates :phone_number, presence: true, format: { with: /\A[0-9]{11}\Z/ }
+  validates :phone_number, presence: true, format: { with: /\A1\d{10}\Z/ }
 
   belongs_to :business_category
 
   before_validation :upcase_id_number
 
-  validate :has_unexpired_appointment
-  validate :in_today
-  validate :in_limitation
+  validate :has_unexpired_appointment,
+           :in_today,
+           :in_limitation,
+           :get_appointment_from_machine
 
   private
 
   def has_unexpired_appointment
-    if Appointment.where("id_number = ? and expired = ?", id_number, false).count > 0
+    if Appointment.where(id_number: id_number, expired: false).exists?
       errors.add(:id_number, "you have unexpired appointment")
     end
   end
@@ -27,9 +35,42 @@ class Appointment < ApplicationRecord
 
   def in_limitation
     limitation = Setting.first&.limitation
-    finished = Appointment.where("appoint_at = ?", Date.today).count
+    finished = Appointment.where(appoint_at: Date.today).count
     if limitation - finished < 0
       errors.add(:id_number, "the people number is reaching limitation")
+    end
+  end
+
+  def get_appointment_from_machine
+    s = Setting.first
+    if s.mip && self.business_category.present?
+      uri = 'http://' + s.mip
+      xml_str = <<-EOF
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <Package>
+          <TranCode>#{s.trans_code}</TranCode>
+          <InstNo>#{s.inst_no}</InstNo>
+          <BizType>#{self.business_category.number}</BizType>
+          <TermNo>#{s.term_no}</TermNo>
+        </Package>
+      EOF
+      xml_data = Nokogiri::XML(xml_str)
+
+      begin
+        # RestClient.post s.mip, xml_data, :content_type => 'application/xml' { |response, request, result|
+        RestClient.get s.mip  { |response, request, result|
+          case response.code
+          when 200
+            response
+          else
+            errors.add(:id_number, "failed to get appointment from machine")
+          end
+        }
+      rescue Exception
+        errors.add(:id_number, "failed to get appointment from machine")
+      end
+    else
+      errors.add(:id_number, "failed to get appointment from machine")
     end
   end
 
