75aa260452213bd279ecc1325786826bb1a49423
dxwang <dxwang68@126.com>
2017-02-07 23:19:25 +0800

    add business categories manage

--- app/controllers/business_categories_controller.rb
+++ app/controllers/business_categories_controller.rb
@@ -1,13 +1,28 @@
 class BusinessCategoriesController < ApplicationController
+  http_basic_authenticate_with name: 'admin', password: 'secret'
+
+  layout 'admin'.freeze
+
   def index
+    @business_categories = BusinessCategory.all
+    @business_category = BusinessCategory.new
   end
 
   def create
-  end
-
-  def show
+    @business_category = BusinessCategory.create business_Category_params
+    render layout: false
   end
 
   def destroy
+    @business_category = BusinessCategory.find params[:id]
+    @business_category.destroy if @business_category
+
+    render layout: false
+  end
+
+  private
+
+  def business_Category_params
+    params.require(:business_category).permit(:name, :prefix, :number)
   end
 end
