9dce35841a1707d6830366b25d0828625d5f4bf6
Lumuy <dxwang68@126.com>
2017-02-11 16:59:49 +0800

    add admin namespace

--- app/controllers/delete_appointment_controller.rb
+++
@@ -1,7 +0,0 @@
-class DeleteAppointmentController < ApplicationController
-  http_basic_authenticate_with name: 'admin', password: 'secret'
-
-  def index
-    @appointments = Appointment.all
-  end
-end
