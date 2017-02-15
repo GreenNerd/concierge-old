b85c2377c194ba3532cd57764827c0114e6d1e6b
Phil Chen <06fahchen@gmail.com>
2017-02-15 16:33:57 +0800

    Skip 0000 serving_number

--- app/models/business_counter.rb
+++ app/models/business_counter.rb
@@ -1,4 +1,6 @@
 class BusinessCounter < ApplicationRecord
+  validate :check_serving_number
+
   after_update_commit :pass_appointments
 
   belongs_to :business_category
@@ -8,5 +10,9 @@ class BusinessCounter < ApplicationRecord
   def pass_appointments
     return if serving_number == '0000'.freeze
     business_category.appointments.where('queue_number < ?', serving_number).update_all expired: true
+  end
+
+  def check_serving_number
+    errors.add(:base, :invalid) if serving_number == '0000'.freeze
   end
 end
