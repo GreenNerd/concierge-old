0694b491963884e363be0c635ec552ae35a02309
Phil Chen <06fahchen@gmail.com>
2017-02-15 15:03:40 +0800

    Pass appointments after business_counter updated

--- app/models/business_counter.rb
+++ app/models/business_counter.rb
@@ -1,3 +1,11 @@
 class BusinessCounter < ApplicationRecord
+  after_update_commit :pass_appointments
+
   belongs_to :business_category
+
+  private
+
+  def pass_appointments
+    business_category.appointments.where('queue_number > ?', serving_number).update_all expired: true
+  end
 end
