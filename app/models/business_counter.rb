a883f2a3df06851a14d7ae15d5651b71fe6c2758
Phil Chen <06fahchen@gmail.com>
2017-02-15 16:29:57 +0800

    Skip pass_appointments if serving_number is '0000'

--- app/models/business_counter.rb
+++ app/models/business_counter.rb
@@ -6,6 +6,7 @@ class BusinessCounter < ApplicationRecord
   private
 
   def pass_appointments
+    return if serving_number == '0000'.freeze
     business_category.appointments.where('queue_number < ?', serving_number).update_all expired: true
   end
 end
