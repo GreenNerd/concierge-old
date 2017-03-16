b4dd0a0fe2d9fee71bc102c51cd7cdc6b97df1b2
Phil Chen <06fahchen@gmail.com>
2017-03-16 15:17:54 +0800

    Disable warmup schedule by env

--- config/initializers/sync_with_machine.rb
+++ config/initializers/sync_with_machine.rb
@@ -1,6 +1,7 @@
-unless defined?(Rails::Console) || File.split($0).last == 'rake' || Rails.env.test?
+unless defined?(Rails::Console) || File.split($PROGRAM_NAME).last == 'rake'.freeze || Rails.env.test? || ENV['DISABLE_SCHEDULE_WARMUP'.freeze]
   # only schedule when not running from the Ruby on Rails console
   # or from a rake task
 
+  Rails.logger.info 'Warmup schedule'.freeze
   Setting.warmup
 end
